0a897cf0c9ca134b3cab106b0f1e6357b3a3fc87
Gokmen Goksel <gokmen@goksel.me>
2011-12-28 13:24:39 +0200

    Add copyright header.

--- ServiceInstaller/main.cpp
+++ ServiceInstaller/main.cpp
@@ -1,3 +1,16 @@
+/****************************************************************************
+** 
+**	Copyright (C) 2011, ServiceInstaller
+**	2011 - SigmaRD ~ Gökmen Göksel <gokmen@goksel.me>
+**	http://github.com/sigmard/ServiceInstaller
+**
+**	This program is free software; you can redistribute it and/or modify it
+**	under the terms of the GNU General Public License as published by the Free
+**	Software Foundation; either version 2 of the License, or (at your option)
+**	any later version.
+**
+****************************************************************************/
+
 #include "serviceinstaller.h"
 #include <QtGui/QApplication>
 
